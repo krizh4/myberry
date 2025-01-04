92ceb64746792f530b13aa51a70a3de74e5ac4bb
krizh4 <krishmikaabisheka1@gmail.com>
2025-01-05 02:19:23 +0530

    MoreChanges

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 mongoose.connect(process.env.DATABASE_URL), {useNewUrlParser: true, useUnifiedTopology: true};
 const db = mongoose.connection
@@ -26,5 +27,6 @@ app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000);
