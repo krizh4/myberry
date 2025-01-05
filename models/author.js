5a096e39b18b830c384984b941b28b74bf1447ba
krizh4 <krishmikaabisheka1@gmail.com>
2025-01-05 14:34:23 +0530

    Finished author routes

--- models/author.js
+++ models/author.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
+const Book = require('./book');
 
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    }
+});
+
+authorSchema.pre('deleteOne', async function (next) {
+    try {
+        const authorId = this.getQuery()._id; // Access the ID of the author being deleted
+        const books = await Book.find({ author: authorId });
+
+        if (books.length > 0) {
+            return next(new Error('This author has books still.'));
+        }
+        next();
+    } catch (err) {
+        next(err);
     }
 });
 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const expresslayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const methodoverride = require('method-override');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expresslayouts);
+app.use(methodoverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
